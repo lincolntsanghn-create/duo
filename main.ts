2e8d896d9951702b582e5ebc4eef5d5441b4ba8e
lincolntsanghn-create <lincoln.tsanghn@gmail.com>
2026-01-03 00:20:30 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,3 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
-})
-controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
-	
-})
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . 3 . . . . . . 
@@ -11,15 +5,16 @@ let mySprite = sprites.create(img`
     . . . . . 3 . . . . . . . . . . 
     . . . . . 3 . 3 . . . . . . . . 
     . . . . . . 3 . . . . . . . . . 
-    . . . . . . 3 . . . . . . . . . 
-    . . . . . . . 3 3 3 3 . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . . . 3 f f f . . . . . . 
+    . . . . . f f 3 3 f 3 . . . . . 
+    . . . . f . . . . f . . . . . . 
+    . . . f . f . . . f . . . . . . 
+    . . . f . f . f f f . . . . . . 
+    . . . f f f . . . . . . . . . . 
+    . . . f . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
+controller.moveSprite(mySprite)
 game.setGameOverMessage(true, "Hello")
